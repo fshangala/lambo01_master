af1162531f8c465ba13810fbe27cccfc06a4ed00
fshangala <fshangala@gmail.com>
2026-02-19 16:15:51 +0200

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness and dark mode support.
    - Implemented utility functions for creating a console and handling command line arguments in `utils.cpp` and `utils.h`.
    - Developed the `Win32Window` class to manage window creation, message handling, and theme updates based on system preferences.
    - Added necessary header files and resource management for the window class.

---
+++ android/app/src/main/kotlin/com/fshangala/apps/lambo01_master/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fshangala.apps.lambo01_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lambo01_master/home.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [], 
+      child: const MyApp()
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lambo01 Master',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+      },
+    );
+  }
+}
